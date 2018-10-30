4676ae5abdf5366705b3af3b9177db04542552b9
David <davidneios25@gmail.com>
2018-10-29 22:05:12 -0400

    deep copy

--- src/components/startHosting/startHosting.js
+++ src/components/startHosting/startHosting.js
@@ -3,32 +3,32 @@ import "./startHosting.scss";
 
 class StartHosting extends Component {
   constructor() {
-      super();
-      this.state = {
-          form: {
-              title: 'mr',
-              first_name: '',
-              last_name: '',
-              email: '',
-              password: ''
-          },
-          termsAgreed: false,
-          formCompleted: false
-      }
-      this.isCompleted = this.isCompleted.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-      this.validateField = this.validateField.bind(this);
+    super();
+    this.state = {
+      form: {
+        title: "mr",
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: ""
+      },
+      termsAgreed: false,
+      formCompleted: false
+    };
+    this.isCompleted = this.isCompleted.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.validateField = this.validateField.bind(this);
   }
   validateField(fieldName) {
-    let form = this.state.form;
-    if(form[fieldName] === ''){
-      this.setState({formCompleted: false})
+    let form = JSON.parse(JSON.stringify(this.state.form));
+    if (form[fieldName] === "") {
+      this.setState({ formCompleted: false });
     }
   }
-  isCompleted(){
-    let invalids = document.querySelectorAll(':invalid');
-    for(let field in this.state.form){
-      if(this.state.form[field] === "" || invalids.length){
+  isCompleted() {
+    let invalids = document.querySelectorAll(":invalid");
+    for (let field in this.state.form) {
+      if (this.state.form[field] === "" || invalids.length) {
         return false;
       }
     }
@@ -36,19 +36,24 @@ class StartHosting extends Component {
   }
   handleChange(evt) {
     const target = evt.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
-    if(target.type === 'checkbox'){
-        this.setState({
-            [name] : !this.state.termsAgreed,
-            formCompleted: this.isCompleted()
-        })
+    if (target.type === "checkbox") {
+      this.setState({
+        [name]: !this.state.termsAgreed,
+        formCompleted: this.isCompleted()
+      });
     } else {
-        let change = Object.assign({}, this.state.form, {[name]: value});
-        this.setState({
-            form: change,
-            formCompleted: this.isCompleted()
-        }, () => {this.validateField(name)});
+      let change = Object.assign({}, this.state.form, { [name]: value });
+      this.setState(
+        {
+          form: change,
+          formCompleted: this.isCompleted()
+        },
+        () => {
+          this.validateField(name);
+        }
+      );
     }
   }
   render() {
@@ -57,43 +62,43 @@ class StartHosting extends Component {
         <h1 className="signup-title">Awesome! Let's dive right in!</h1>
         <form action="index.html" className="signup-form">
           <label htmlFor="title">Title</label>
-          <select 
-            id="title" 
-            name='title' 
+          <select
+            id="title"
+            name="title"
             value={this.state.form.title}
-            onChange={this.handleChange}  
+            onChange={this.handleChange}
           >
             <option value="mr">Mr.</option>
             <option value="ms">Ms.</option>
           </select>
           <label htmlFor="first-name">First name</label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             id="first-name"
             name="first_name"
             onChange={this.handleChange}
             value={this.state.form.first_name}
           />
           <label htmlFor="last-name">Last name</label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             id="last-name"
             name="last_name"
             onChange={this.handleChange}
             value={this.state.form.last_name}
           />
           <label htmlFor="email">E-Mail</label>
-          <input 
-            type="email" 
+          <input
+            type="email"
             id="email"
             name="email"
             onChange={this.handleChange}
             value={this.state.form.email}
           />
           <label htmlFor="password">Password</label>
-          <input 
-            type="password" 
-            id="password" 
+          <input
+            type="password"
+            id="password"
             name="password"
             onChange={this.handleChange}
             value={this.state.form.password}
@@ -109,13 +114,13 @@ class StartHosting extends Component {
             />
             <label htmlFor="agree-terms">
               Agree to
-            <a>Terms &amp; Conditions</a>
+              <a>Terms &amp; Conditions</a>
             </label>
           </div>
-          
-          <input 
-            type="submit" 
-            className="button" 
+
+          <input
+            type="submit"
+            className="button"
             value="Sign Up"
             disabled={!(this.state.formCompleted && this.state.termsAgreed)}
           />
